6c55bcf9fc66252f72cb060d1c4b6b6455835996
Andon Andonov <andon.andonov@gmail.com>
2013-02-27 21:33:01 +0200

    add problem 1 of oop principles part 1 homework

---
+++ 3.OOP/4.OOP-Principles-Part-I/OOPPrinciplesPartOne/School/Teacher.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+
+public class Teacher : Human, ICommentable
+{
+    //Fields
+    private List<Discipline> disciplines;
+    private List<string> comments;
+
+    //Properties
+    /// <summary>
+    /// Gets an array of the disciplines a given teacher teaches
+    /// </summary>
+    public Discipline[] Disciplines
+    {
+        get
+        {
+            return this.disciplines.ToArray();
+        }
+        private set 
+        {
+        }
+    }
+
+    /// <summary>
+    /// Get the comments for a student
+    /// </summary>
+    public string[] Comments
+    {
+        get
+        {
+            return this.comments.ToArray();
+        }
+    }
+
+    //Constructors
+    /// <summary>
+    /// Create new teacher
+    /// </summary>
+    /// <param name="name">The name of the teacher</param>
+    /// <param name="disciplines">Array of the disciplines the teacher can teach</param>
+    public Teacher(string name, Discipline[] disciplines)
+        :base(name)
+    {
+        this.disciplines = new List<Discipline>(disciplines);
+        this.comments = new List<string>();
+    }
+
+    //Methods
+    /// <summary>
+    /// Add new discipline that given teacher can teach
+    /// </summary>
+    public void AddDiscipline(Discipline discipline) 
+    {
+        this.disciplines.Add(discipline);
+    }
+
+    /// <summary>
+    /// Removes a discipline from a teachers list
+    /// </summary>
+    /// <param name="discipline"></param>
+    public void RemoveDiscipline(Discipline discipline)
+    {
+        this.disciplines.Remove(discipline);
+    }
+
+    /// <summary>
+    /// Add comment
+    /// </summary>
+    public void AddComment(string comment)
+    {
+        this.comments.Add(comment);
+    }
+}
+
---
+++ 3.OOP/4.OOP-Principles-Part-I/OOPPrinciplesPartOne/School/TestApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class TestApp
+{
+    static void Main()
+    {
+        Student stud1 = new Student("Mario", 1);
+        Student stud2 = new Student("Batman", 4);
+        Student stud3 = new Student("Optimus  Prime", 3);
+        Student[] students = new Student[] { stud1, stud2, stud3};
+
+        Discipline disp1 = new Discipline("OOP", 40, 40);
+
+        Teacher teach1 = new Teacher("Morgan Freeman", new Discipline[] { disp1 });
+        Teacher[] teachers = new Teacher[] {teach1};
+
+        Class class1 = new Class(students, teachers, "12A");
+
+        Console.Write("The name of the first discipline of the first teacher of the first class: ");
+        Console.WriteLine(class1.Teachers[0].Disciplines[0].Name);
+
+
+        stud1.AddComment("ninja");
+        Console.Write("The first comment for the first studdent: ");
+        Console.WriteLine(stud1.Comments[0]);
+    }
+}
+
